a62b7dd0454bb05d5ba40f50b33ea9e009e4e60d
romain <romain.gadrat@epitech.eu>
2017-10-08 21:15:18 +0200

    (plugin) AddrMac   - Parsing the msg.
    (plugin) Key
      - Parsing the msg.

--- msu/src/plugin/addrMac.cpp
+++ msu/src/plugin/addrMac.cpp
@@ -11,7 +11,11 @@ bool AddrMac::test(std::string res) {
   return (res.compare(0, name.length(), name) == 0);
 }
 
-int AddrMac::exec(mongocxx::database, Session *, std::string) {
-  std::cout << "addr_mac" << std::endl;
+int AddrMac::exec(mongocxx::database, Session * session, std::string msg) {
+  std::string mac = msg.substr(msg.find(" ") + 1);
+  mac.erase(mac.size() - 1);
+
+  std::cout << "The client [" << session->getUniqueId() << "] is using : ";
+  std::cout << "addr_mac [" << mac << "]" << std::endl;
   return (0);
 }
--- msu/src/plugin/key.cpp
+++ msu/src/plugin/key.cpp
@@ -11,7 +11,11 @@ bool Key::test(std::string res) {
   return (res.compare(0, name.length(), name) == 0);
 }
 
-int Key::exec(mongocxx::database, Session *, std::string) {
-  std::cout << "Key" << std::endl;
+int Key::exec(mongocxx::database, Session * session, std::string msg) {
+  std::string theKey = msg.substr(msg.find(" ") + 1);
+  theKey.erase(theKey.size() - 1);
+
+  std::cout << "The client [" << session->getUniqueId() << "] is using : ";
+  std::cout << "Key [" << theKey << "]" << std::endl;
   return (0);
 }
