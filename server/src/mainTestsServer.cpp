b234d1b8f3d7231e578b72568e6316f8b607bb22
Liroo <pierre-monge@hotmail.fr>
2017-10-08 17:43:47 +0200

    (feature) timestamp
    - add timestamp plugin

--- server/src/mainTestsServer.cpp
+++ server/src/mainTestsServer.cpp
@@ -4,11 +4,15 @@
 #include <boost/asio.hpp>
 #include "msu.hpp"
 #include "plugin/key.hpp"
+#include "plugin/timestamp.hpp"
 
 int main() {
   MSU::Core core(1024);
+  Timestamp *pluginTimestamp;
   Key *pluginKey;
 
+  pluginTimestamp = new Timestamp();
+  core.use(pluginTimestamp, PLUGIN_ENDPOINT);
   pluginKey = new Key();
   core.use(pluginKey, PLUGIN_ENDPOINT);
   core.run();
